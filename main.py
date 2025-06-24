4e20c98b2a763d8da79577c4f1602948b5f4680a
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 15:14:44 -0500

    fix: workflow
    autodeploy to Ec2

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def create_comment_route():
         return jsonify({"error": f"Server error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
