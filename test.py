ecf49ebd868476b8205a91ba85890a5087902b0e
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 15:19:14 -0500

    test: to deploy to Ec2

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests
 
 login_data = {
-    "User_mail": "ascorread1",
+    "User_mail": "allan",
     "password": "1234"
 }
-login_response = requests.post("http://localhost:8080/login", json=login_data)
+login_response = requests.post("http://52.203.72.116:8080/login", json=login_data)
 
 if login_response.status_code != 200:
     print("Error al iniciar sesión:", login_response.status_code, login_response.text)
@@ -14,7 +14,7 @@ token = login_response.json()["token"]
 print("Token:", token)
 
 comment_data = {
-    "Id_publication": "68576f513ac5b69dd669e328",
+    "Id_publication": "6858bb431ed97d52a0c6c02f",
     "Comment": "Test comment by the developer"
 }
 
@@ -22,7 +22,7 @@ headers = {
     "Authorization": f"Bearer {token}"
 }
 
-response = requests.post("http://localhost:8081/create-comment", json=comment_data, headers=headers)
+response = requests.post("http://3.221.235.171:8080/create-comment", json=comment_data, headers=headers)
 
 print("Status:", response.status_code)
 print("Response:", response.json())
